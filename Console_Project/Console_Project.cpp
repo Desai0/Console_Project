968442d1405114c28701e301fd203653459fb011
Desai0 <kolobokevgenij83@gmail.com>
2025-01-15 09:08:26 +0300

    Добавил заголовки

---
+++ Console_Project/Console_Project.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include "sqlite3.h"
+
+int main()
+{
+    setlocale(0, "");
+    sqlite3* db;
+    int rc;
+    rc = sqlite3_open("database", &db);
+
+    if (rc) {
+        std::cerr << "Невозможно открыть бд: " << sqlite3_errmsg(db) << std::endl;
+        return 1;
+    }
+    else {
+        std::cout << "Успешно открыта БД " << std::endl;
+    }
+
+    sqlite3_close(db);
+
+    return 0;
+}
